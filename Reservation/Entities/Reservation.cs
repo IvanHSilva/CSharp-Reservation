d682d086edf1b698488554b54701e9d79881c3dd
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-03 15:22:47 -0300

    Class created

---
+++ Reservation/Entities/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reservation.Entities {
+    public class Reservation {
+        // Attributes
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        // Constructors
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout) {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        // Methods
+        public int Duration() {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout) {}
+
+        public override string ToString() {
+            return $"Quarto {RoomNumber}, CheckIn em {CheckIn.ToString("dd/MM/yyyy")}, " +
+                $"CheckOut em {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} noites";
+        }
+    }
+}
