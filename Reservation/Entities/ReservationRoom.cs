c35da8b8619a2e991f56b4e8abc24746b389a1d3
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-03 16:00:55 -0300

    Program made with if-else solution

--- Reservation/Entities/ReservationRoom.cs
+++ Reservation/Entities/ReservationRoom.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 
 namespace Reservation.Entities {
-    public class Reservation {
+    public class ReservationRoom {
         // Attributes
         public int RoomNumber { get; set; }
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
 
         // Constructors
-        public Reservation() { }
+        public ReservationRoom() { }
 
-        public Reservation(int roomNumber, DateTime checkin, DateTime checkout) {
+        public ReservationRoom(int roomNumber, DateTime checkin, DateTime checkout) {
             RoomNumber = roomNumber;
             CheckIn = checkin;
             CheckOut = checkout;
@@ -22,7 +22,10 @@ namespace Reservation.Entities {
             return (int)duration.TotalDays;
         }
 
-        public void UpdateDates(DateTime checkin, DateTime checkout) {}
+        public void UpdateDates(DateTime checkin, DateTime checkout) {
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
 
         public override string ToString() {
             return $"Quarto {RoomNumber}, CheckIn em {CheckIn.ToString("dd/MM/yyyy")}, " +
--- Reservation/Program.cs
+++ Reservation/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
+using Reservation.Entities;
 
 namespace Reservation {
     internal class Program {
         static void Main(string[] args) {
+            Console.Write("Quarto nº: ");
+            int room = int.Parse(Console.ReadLine());
+            Console.Write("Checkin (Entrada): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Checkout (Saída): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+            if (checkout <= checkin) {
+                Console.WriteLine("Erro: Chekout deve sair maior que Checkin!");
+                return;
+            }
+            ReservationRoom reservation = new ReservationRoom(room, checkin, checkout);
+            Console.WriteLine(reservation);
+            Console.WriteLine("");
+
+            Console.WriteLine("Dados atualizados:");
+            Console.Write("Checkin (Entrada): ");
+            checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Checkout (Saída): ");
+            checkout = DateTime.Parse(Console.ReadLine());
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now) {
+                Console.WriteLine("Erro: Checkin e Checkout devem ser datas futuras!");
+                return;
+            } else if (checkout <= checkin) {
+                Console.WriteLine("Erro: Chekout deve sair maior que Checkin!");
+                return;
+            }
+            reservation.UpdateDates(checkin, checkout);
+            Console.WriteLine(reservation);
+            Console.WriteLine();
         }
     }
 }
